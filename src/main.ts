aed5ff05e4e30ef18bedc1f96d5ab18e19b05b8a
boat.suka <boat.suka@gmail.com>
2022-06-09 13:01:27 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  await app.setGlobalPrefix('api/v1');
   await app.enableCors();
   await app.listen(process.env.port || 3000);
 }
